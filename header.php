7df533c589b9ce1aadcd42bd6597549c7ea9338d
bwaltergraphics <55985711+bwaltergraphics@users.noreply.github.com>
2019-12-12 19:57:44 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i|Solway:400,700&display=swap" rel="stylesheet">
+
+
+        <?php wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+        wp_head();
+        ?>
+
+    </head>
+
+    <body>
+        <header>
+            <div class="one-col-container">
+                <div class="logo">Logo</div>
+                <nav id="menu">
+                        <ul>
+                          <li><a href="#">Clean Energy Education</a></li>
+                          <li><a href="#">Careers</a></li>
+                          <li><a href="#">Resources</a></li>
+                          <li><a href="#">Contact Form</a></li>
+                        </ul>
+                </nav>
+            </div> 
+
+
+        </header>
